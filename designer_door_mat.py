ed5209fde1b36293aa90465cd325be3880b7977f
Gagan <107067282+gagankrpandit@users.noreply.github.com>
2022-09-16 19:41:51 +0530

    Update designer_door_mat.py

--- designer_door_mat.py
+++ designer_door_mat.py
@@ -1,19 +1,19 @@
 import math
-N = int(input())
-if N % 2 != 0:
-    M = N * 3
+N, M = input().split()
+if int(N) % 2 != 0:
+    # M = N * 3
     h = '-'
     d = '.'
     p = '|'
     design = f'{d}{p}{d}'
-    for i in range(N):
+    for i in range(int(N)):
         if i == 0:
             continue
         elif i % 2 != 0:
-            print((design*(i)).center(M, '-'))
+            print((design*(i)).center(int(M), '-'))
 
-    for i in range(N,0,-1):
-        if i == N:
-            print('WELCOME'.center(M, '-'))
+    for i in range(int(N),0,-1):
+        if i == int(N):
+            print('WELCOME'.center(int(M), '-'))
         elif i % 2 != 0:
-            print((design*(i)).center(M, '-'))
+            print((design*(i)).center(int(M), '-'))
